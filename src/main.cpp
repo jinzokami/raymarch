fbc28b585b26faedfb9089919186bc50eaf5fc21
gilberto veras <gilberto.veras@aol.com>
2019-09-04 14:33:56 -0400

    beginnings of image output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include "string.h"
+#include "stdint.h"
+
 #include "stb_image.h"
 
 int main(int argc, char** argv)
 {
+    const unsigned int width = atoi(argv[1]);
+    const unsigned int height = atoi(argv[2]);
+
+    char* pixels = (char*)malloc(sizeof(char)*width*height);
+
+    free(pixels);
+    
     return 0;
 }
