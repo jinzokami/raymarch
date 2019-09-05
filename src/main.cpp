9c085b31c556c768797f0f5d2bf4dd25d2cd9c41
gilberto veras <gilberto.veras@aol.com>
2019-09-04 23:57:38 -0400

    playing around with pixels

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,30 @@
 #include "string.h"
 #include "stdint.h"
+#include "stdlib.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+struct pixel
+{
+    unsigned char r, g, b;
+};
+
 int main(int argc, char** argv)
 {
-    const unsigned int width = 256;//atoi(argv[1]);
-    const unsigned int height = 256;//atoi(argv[2]);
+    const unsigned int width = 1024;//atoi(argv[1]);
+    const unsigned int height = 1024;//atoi(argv[2]);
 
-    unsigned char* pixels = (unsigned char*)malloc(sizeof(char)*3*width*height);
+    pixel* pixels = (pixel*)malloc(sizeof(pixel)*width*height);
 
-    for (int i = 0; i < 3*width*height; i+=3)
+    for (int x = 0; x < width; x++)
     {
-        pixels[i] = 255;
-        pixels[i+1] = 255;
-        pixels[i+2] = 255;
+        for (int y = 0; y < height; y++)
+        {
+            pixels[(y * width + (x % width))] = {x, 0, 255-x};
+        }
     }
 
     stbi_write_bmp("out.bmp", width, height, 3, pixels);
